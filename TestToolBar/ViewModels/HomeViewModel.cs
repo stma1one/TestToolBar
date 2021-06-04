ad77904c8b518304c19c14964e9950b0e978053d
Home <Home@LAPTOP-2E6V6ACM>
2021-06-04 10:29:39 +0300

    Add project files.

---
+++ TestToolBar/ViewModels/HomeViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestToolBar.Views;
+using Xamarin.Forms;
+using System.Windows.Input;
+using System.ComponentModel;
+
+namespace TestToolBar.ViewModels
+{
+    class HomeViewModel : INotifyPropertyChanged
+    {
+        #region members
+        private int count;
+        #endregion
+
+        #region properties
+        public int Count { get { return count; } 
+        set { if (count != value)
+                { count = value;
+                    //when Count is updated ->check if the CanChange function output is changed
+                    (Display as Command).ChangeCanExecute();
+                    OnPropertyChange(nameof(Count)); } } }
+        #endregion
+
+        #region INOTIFYPROPERTYCHANGED
+        #region event
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+        private void OnPropertyChange(string v)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+        #endregion
+
+
+        #region Commands
+        public ICommand PressMe { get; set; }
+
+        public ICommand Display { get; set; }
+        #region CanExecute 
+        private bool CanChange()
+        {
+            return Count%3==0;
+        }
+        #endregion
+        #endregion
+
+        #region Constructor
+        public HomeViewModel() 
+        { count = 1;
+            Display = new Command(() => { Application.Current.MainPage.DisplayAlert("Hello", "Hi there", "OK"); },CanChange);
+            PressMe = new Command(() => { Count++; });
+        }
+        #endregion
+
+    }
+}
